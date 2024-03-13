27a11f7bc4b25b6d22b7dc6c79b51506dcd44b41
Ryanyan89 <ryanyan1404@hotmail.com>
2024-03-13 12:18:44 -0700

    Added the layout of the Main recipes page. It is currently ONLY a layout as the Tasty API has not been added. As mentioned in the code there will be areas that need to be changed once the Tasty API is implemented. Linking to different pages has also not been added so everything is currently in the App.js to see the layout

--- project/src/App.js
+++ project/src/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Container, Row, Col } from 'react-bootstrap';
+import { NavBar } from './Basic_Layout_Components/NavBar'
+import { RecipeOfTheDay } from './Recipes_Page_Components/Today_Recipe';
+import { TrendingRecipe } from './Recipes_Page_Components/Trending_Recipes';
 
+//Idk how to do link pages so everything is here so I can see the page
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button>test</Button>
-      </header>
-    </div>
+    <>
+      <NavBar/>
+      <Container className='mt-4'>
+        <Row style={{ justifyContent: 'center' }}>
+          <h4>Recipe of the day</h4>
+          <RecipeOfTheDay />
+        </Row>
+        <Row>
+          <h5>Trending_Recipes</h5>   
+          <Col><TrendingRecipe /></Col>
+          <Col><TrendingRecipe /></Col>
+          <div className="w-100"></div>
+          <Col><TrendingRecipe /></Col>
+          <Col><TrendingRecipe /></Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
---
+++ project/src/Basic_Layout_Components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export function NavBar() {
+    return (
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container className='ms-1'>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        {/*The links do not go anywhere current */}
+                        <Nav.Link href="#home">Healthy Recipes</Nav.Link>
+                        <Nav.Link href="#features">Exercise Assistance</Nav.Link>
+                    </Nav>
+                </Container>
+                <Container style={{justifyContent: 'end'}}>
+                    {/*The form current does nothing nor does*/}
+                    <Form inline>
+                        <Row>
+                            <Col xs="auto" >
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Search"
+                                    className=" mr-sm-2"
+                                />
+                            </Col>
+                            <Col xs="auto">
+                                <Button type="submit">Submit</Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Container>
+            </Navbar>
+        </>
+    );
+}

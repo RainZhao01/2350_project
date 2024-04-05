91f74609c70e03894d91d2505b5bde6f53f851da
John <johnjjbaltazar@icloud.com>
2024-04-05 15:25:33 -0700

    Backend calculations for breakfast,lunch,and dinner are working. Now working on Displaying to page

--- src/pages/Recipe/MealPlan.jsx
+++ src/pages/Recipe/MealPlan.jsx
@@ -18,7 +18,7 @@ export function MealPlanPage(){
         console.log('Calorie Goal:', calorieGoal);
 
         setCalorieGoal(calorieGoal);
-
+// -----------------------------------   BREAKFAST   -------------------------------------
         const findSuitableBreakfastRecipe = (recipes, calorieGoal) => {
             const breakfastCals = 0.25 * calorieGoal;
         
@@ -51,7 +51,7 @@ export function MealPlanPage(){
 
             const suitableBreakfastRecipe = findSuitableBreakfastRecipe(breakfastData.results, calorieGoal);
             if (suitableBreakfastRecipe) {
-                console.log("This is the recipe:", suitableBreakfastRecipe);
+                console.log("This is the breakfast recipe:", suitableBreakfastRecipe);
                 setBreakfastRecipes(suitableBreakfastRecipe);
             } else {
                 console.log("No suitable breakfast recipe found.");
@@ -59,90 +59,89 @@ export function MealPlanPage(){
         } catch (error) {
             console.error('Error fetching data: ', error);
         }
+// -----------------------------------   LUNCH   -------------------------------------
+        const findSuitableLunchRecipe = (recipes, calorieGoal) => {
+            const lunchCals = 0.30 * calorieGoal;
 
-        let breakfastCals = 0.25 * calorieGoal;
-        let lunchCals = 0.35 * calorieGoal;
-        let dinnerCals = 0.35 * calorieGoal;
-        console.log("Breakfast CalGoal, " + breakfastCals + " Breakfast CalGoal, " + lunchCals + " Breakfast CalGoal, " + dinnerCals);
-    };
+            return recipes.find(recipe => {
+                const { nutrition } = recipe;
+                if (nutrition) {
+                    const { calories } = nutrition;
+                    if (calories) {
+                        const lowerBound = lunchCals - 100;
+                        const upperBound = lunchCals + 100;
+                        return calories >= lowerBound && calories <= upperBound;
+                    }
+                }
+                return false;
+            });
+        };
 
-    // const findSuitableBreakfastRecipe = (recipes, calorieGoal) => {
-    //     const breakfastCals = 0.25 * calorieGoal;
+        try {
+            const randomRecipeStart = Math.floor(Math.random() * 100);
+            const lunchRecipesURL = `https://tasty.p.rapidapi.com/recipes/list?from=${randomRecipeStart}&size=30&tags=meal,lunch`;
 
-    //     return recipes.find(recipe => {
-    //         const { nutrition } = recipe;
-    //         const { calories } = nutrition;
-    //         const lowerBound = breakfastCals - 100;
-    //         const upperBound = breakfastCals + 100;
-    //         return calories >= lowerBound && calories <= upperBound;
-    //     });
-    // };
+            const lunchResponse = await fetch(lunchRecipesURL, {
+                headers: {
+                    'X-RapidAPI-Key': '99c5b37348mshcd1a26a64153451p1b2fc0jsne5ca216d0e61',
+                    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+                }
+            });
+            const lunchData = await lunchResponse.json();
+            console.log(lunchData);
 
-    // useEffect(() => {
-    //     const fetchRecipes = async () => {
-    //         //breakfast
-    //         try {
-    //             const randomRecipeStart = Math.floor(Math.random() * 100);
-    //             const breakfastRecipes = `https://tasty.p.rapidapi.com/recipes/list?from=${randomRecipeStart}&size=10&tags=meal,breakfast`;
+            const suitableLunchRecipe = findSuitableLunchRecipe(lunchData.results, calorieGoal);
+            if (suitableLunchRecipe) {
+                console.log("This is the lunch recipe:", suitableLunchRecipe);
+                setBreakfastRecipes(suitableLunchRecipe);
+            } else {
+                console.log("No suitable lunch recipe found.");
+            }
+        } catch (error) {
+            console.error('Error fetching data: ', error);
+        }
+// -----------------------------------   DINNER   -------------------------------------
+        const findSuitableDinnerRecipe = (recipes, calorieGoal) => {
+            const dinnerCals = 0.30 * calorieGoal;
 
-    //             const breakfastResponse = await fetch(breakfastRecipes, {
-    //                 headers: {
-    //                     'X-RapidAPI-Key': '99c5b37348mshcd1a26a64153451p1b2fc0jsne5ca216d0e61',
-    //                     'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-    //                 }
-    //             });
-    //             const breakfastData = await breakfastResponse.json();
-    //             console.log(breakfastData);
-    //             const suitableBreakfastRecipe = findSuitableBreakfastRecipe(breakfastData.results, calorieGoal);
-    //             if (suitableBreakfastRecipe) {
-    //                 console.log("This is the recipe:", suitableBreakfastRecipe);
-    //                 setBreakfastRecipes(suitableBreakfastRecipe);
-    //             } else {
-    //                 console.log("No suitable breakfast recipe found.");
-    //             }
-    //             console.log(suitableBreakfastRecipe);
-    //         } catch (error) {
-    //             console.error('Error fetching data: ', error);
-    //         }
-    //         // lunch call
-    //         try {
-    //             const randomRecipeStart = Math.floor(Math.random() * 100);
-    //             const lunchRecipes = `https://tasty.p.rapidapi.com/recipes/list?from=${randomRecipeStart}&size=10&tags=meal,lunch`;
+            return recipes.find(recipe => {
+                const { nutrition } = recipe;
+                if (nutrition) {
+                    const { calories } = nutrition;
+                    if (calories) {
+                        const lowerBound = dinnerCals - 100;
+                        const upperBound = dinnerCals + 100;
+                        return calories >= lowerBound && calories <= upperBound;
+                    }
+                }
+                return false;
+            });
+        };
 
-    //             const lunchResponse = await fetch(lunchRecipes, {
-    //                 headers: {
-    //                     // 'X-RapidAPI-Key': '99c5b37348mshcd1a26a64153451p1b2fc0jsne5ca216d0e61',
-    //                     'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-    //                 }
-    //             });
-    //             const lunchData = await lunchResponse.json();
-    //             setBreakfastRecipes(lunchData);
-    //             console.log(lunchData);
-    //         } catch (error) {
-    //             console.error('Error fetching data: ', error);
-    //         }
-    //         //dinner
-    //         try {
-    //             const randomRecipeStart = Math.floor(Math.random() * 100);
-    //             const dinnerRecipes = `https://tasty.p.rapidapi.com/recipes/list?from=${randomRecipeStart}&size=10&tags=meal,dinner`;
+        try {
+            const randomRecipeStart = Math.floor(Math.random() * 100);
+            const dinnerRecipesURL = `https://tasty.p.rapidapi.com/recipes/list?from=${randomRecipeStart}&size=30&tags=meal,dinner`;
 
-    //             const dinnerResponse = await fetch(dinnerRecipes, {
-    //                 headers: {
-    //                     // 'X-RapidAPI-Key': '99c5b37348mshcd1a26a64153451p1b2fc0jsne5ca216d0e61',
-    //                     'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-    //                 }
-    //             });
-    //             const dinnerData = await dinnerResponse.json();
-    //             setBreakfastRecipes(dinnerData);
-    //             console.log(dinnerData);
-    //         } catch (error) {
-    //             console.error('Error fetching data: ', error);
-    //         }
-    //     };
+            const dinnerResponse = await fetch(dinnerRecipesURL, {
+                headers: {
+                    'X-RapidAPI-Key': '99c5b37348mshcd1a26a64153451p1b2fc0jsne5ca216d0e61',
+                    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+                }
+            });
+            const dinnerData = await dinnerResponse.json();
+            console.log(dinnerData);
 
-    //     fetchRecipes();
-    // }, []);
-      
+            const suitableDinnerRecipe = findSuitableDinnerRecipe(dinnerData.results, calorieGoal);
+            if (suitableDinnerRecipe) {
+                console.log("This is the dinner recipe:", suitableDinnerRecipe);
+                setBreakfastRecipes(suitableDinnerRecipe);
+            } else {
+                console.log("No suitable dinner recipe found.");
+            }
+        } catch (error) {
+            console.error('Error fetching data: ', error);
+        }
+    }
 
     return(
         <Container className='mt-4'>
